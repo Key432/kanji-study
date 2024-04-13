34647912990d5616edcd9fc4c641e65efb13ce9b
Key <smz432ksn@icloud.com>
2024-04-14 01:39:46 +0900

    update: リポジトリを変更

--- src/components/base/footer/Footer.tsx
+++ src/components/base/footer/Footer.tsx
@@ -5,9 +5,9 @@ export function Footer() {
     <footer className="w-full h-12 bg-primary-300">
       <div className="h-full container mx-auto flex justify-end items-center">
         <Link
-          href="https://github.com/Key432/key-kanji-note"
+          href="https://github.com/Key432/kanji-study"
           target="_blank"
-          className="active:translate-y-0.5"
+          className="px-4 active:translate-y-0.5"
         >
           リポジトリ
         </Link>
